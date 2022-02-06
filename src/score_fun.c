aac6a89cf20eee13c7ab4d44c9d44e15985be6a0
Katia Bulekova <ktrn@bu.edu>
2022-02-05 19:06:46 -0500

    Clean and document score_fun.c

--- src/score_fun.c
+++ src/score_fun.c
@@ -6,24 +6,49 @@
 #include <Rmath.h>              
 
 
-struct ks_score{
-   double score;
-   double pvalue;
+
+// Define a structure, so we could pass it back to R
+struct ks_score
+{
+  double score;
+  double pvalue;
 };
 
+
+// -------------------------------------------- //
+//
+// swap
+// Swap two elements in an array
+//
+// INPUT & OUTPUT:
+//   int *a
+//   int *b
+// --------------------------------------------- //
 static
-void swap(int* a, int* b) 
-{ 
+  void swap(int* a, int* b) 
+  { 
     int t = *a; 
     *a = *b; 
     *b = t; 
-} 
+  } 
 
 
+// -------------------------------------------- //
+//
+// cadra_pkstwo
+// Simplified implementation of pkstwo function
+// 
+// INPUT:
+//    x
+//    tol - tolerance (default: = 1e-6)
+// 
+// OUTPUT:
+//    \sum_{-\infty}^\infty (-1)^k e^{-2k^2x^2}
+// -------------------------------------------- //
 
 static double
-cadra_pkstwo(double x, double tol)
-{
+  cadra_pkstwo(double x, double tol)
+  {
     /* This is a simple case of the original pkstwo function for n = 1*/
     double new, old, s, w, z;
     int k, k_max;
@@ -55,88 +80,156 @@ cadra_pkstwo(double x, double tol)
       res = new;
     }
     return(res);
-}
+  }
 
 
-
+// -------------------------------------------- //
+// 
+// partition
+// Swap elements if the left element is larger than the right element
+// 
+// INPUT:
+//    arr - an array to be sorted
+//    order - an array with the order of the eleents
+//    low - left position
+//    high - right position
+//
+// OUTPUT:
+//    arr - an array to be sorted
+//    order - an array with the order of the eleents
+// -------------------------------------------- //
 
 static
-int partition (int *arr, int *order, int low, int high) 
-{ 
+  int partition (int *arr, int *order, int low, int high) 
+  { 
     int pivot = arr[high]; // pivot 
     int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
     int j; 
- 
+    
     for (j = low; j <= high - 1; j++) 
     { 
-        // If current element is smaller than the pivot 
-        if (arr[j] < pivot) 
-        { 
-            i++; // increment index of smaller element 
-            swap(&arr[i], &arr[j]); 
-            swap(&order[i], &order[j]);
-        } 
+      // If current element is smaller than the pivot 
+      if (arr[j] < pivot) 
+      { 
+        i++; // increment index of smaller element 
+        swap(&arr[i], &arr[j]); 
+        swap(&order[i], &order[j]);
+      } 
     } 
     swap(&arr[i + 1], &arr[high]); 
     swap(&order[i+1], &order[high]);
     return (i + 1); 
-} 
-  
+  } 
+
+
+// -------------------------------------------- //
+// 
+// quickSort
+// Quick Sort of an integer array
+//
+// INPUT:
+//    arr - input array
+//    order - initial order
+//    low - left position within array
+//    high - right position within array
+//
+// OUTPUT:
+//    array - sorted array
+//    order - order after the sorting
+// -------------------------------------------- //
 static
-void quickSort(int *arr, int *order, int low, int high) 
-{ 
+  void quickSort(int *arr, int *order, int low, int high) 
+  { 
     if (low < high) 
     { 
-        /* pi is partitioning index, arr[p] is now 
-        at right place */
-        int pi = partition(arr, order, low, high); 
-  
-        // Separately sort elements before 
-        // partition and after partition 
-        quickSort(arr, order, low, pi - 1); 
-        quickSort(arr, order, pi + 1, high); 
+      /* pi is partitioning index, arr[p] is now 
+       at right place */
+      int pi = partition(arr, order, low, high); 
+      
+      // Separately sort elements before 
+      // partition and after partition 
+      quickSort(arr, order, low, pi - 1); 
+      quickSort(arr, order, pi + 1, high); 
     } 
-} 
+  } 
 
+
+// ---------------------------------------------- //
+//                                                //
+// match_subarray
+//
+//  Find position of array2 within array 1
+//
+// INPUT:
+//    N - number of elements in array 1
+//    array1 
+//    array2
+//
+// OUTPUT:
+//   res - resulting array with positions
+// ------------------------------------------------ //
 static
-void match_subarray( int N, int* array1, int* array2, int* res)
-{
+  void match_subarray( int N, int* array1, int* array2, int* res)
+  {
     int* ptr = array1;
     int* ptr2 = array2;
-
+    
     int i=0;
-
+    
     while(ptr < array1 + N) {
-        if(*ptr == *ptr2) {
-            ptr++;
-            res[i++] = ptr2 -array2;
-        }
-        ptr2++;
+      if(*ptr == *ptr2) {
+        ptr++;
+        res[i++] = ptr2 -array2;
+      }
+      ptr2++;
     }
-   return;
-}
+    return;
+  }
 
+
+// ---------------------------------------------- //
+//                                                //
+// make_Y_array
+//
+// Prepare Y array for computing KS score
+//
+// INPUT:
+//   N - number of elements in the input y array
+//   y - array with integers
+//
+// OUTPUT:
+//   y2 - output array
+// ----------------------------------------------- //
 
 static
-int make_Y_array( int N, int* y, int* y2 ) 
-{
-   int i, j=2;
+  int make_Y_array( int N, int* y, int* y2 ) 
+  {
+    int i, j=2;
+    
+    y2[0] = y[0] - 1;
+    y2[1] = y[0];
+    for ( i = 1; i < N; i++)
+    {
+      if(y[i] - 1 == y2[j-1])
+        y2[j++] = y[i];
+      else
+      {
+        y2[j++] = y[i] - 1;
+        y2[j++] = y[i];
+      }   
+    }
+    
+    return(j);
+  } 
 
-   y2[0] = y[0] - 1;
-   y2[1] = y[0];
-   for ( i = 1; i < N; i++)
-   {
-       if(y[i] - 1 == y2[j-1])
-          y2[j++] = y[i];
-       else
-       {
-          y2[j++] = y[i] - 1;
-          y2[j++] = y[i];
-       }   
-   }
- 
-   return(j);
-} 
+// ---------------------------------------------- //
+//                                                //
+// ks_test_simple
+//
+// Simple implementation of Kolmogorov-Smirnov test
+// score computation
+//
+//
 static
   double ks_test_simple( int n_x, int n_y, int* y, int alt)
   {
@@ -161,6 +254,7 @@ static
       order[i] = i+1;
     }
     
+    // Calculate order of the elements in w array
     quickSort( w, order, 0, n_x + n_y - 1);
     
     for (i = 1; i < n_x + n_y; i++){
@@ -213,11 +307,26 @@ static
     return (result);
   }
 
-
-
+// ---------------------------------------------- //
+//                                                //
+// ks_genscore
+//
+// INPUT:
+// int n_x
+// int n_y - number of values in y array
+// int* y  
+// int n_w - number of elements in weight 
+// int* weight - an integer array of weights
+//
+// OUTPUT:
+//   ks_score structure: ks score and p-value
+//
+// int alt:   0: "two.sided", +1: "less"; -1: "greater"
+// ----------------------------------------------------
 static
-  struct ks_score ks_genscore( int n_x, int n_y, int* y, int n_w, int* weight, int alt)
-  /* +1 = left-sided, 0 - double-sided, -1 = right-sided */
+  struct ks_score 
+  ks_genscore( int n_x, int n_y, int* y, int n_w, int* weight, int alt)
+  /* +1 = left-sided, 0 - two-sided, -1 = right-sided */
   {
     struct ks_score res;
     
@@ -280,12 +389,10 @@ static
       for (i = 0; i < n_y; i++) D[ y_match[i] ] = i + 1;
       for (i = 1; i < y2_N; i++) if (D[i] == 0) D[i] = D[i-1];
       for (i = 0; i < y2_N; i++) {
-        
         if( fabs(D[i] * hit - y2[i] * mis) > zmax ) {
           zmax = fabs(D[i] * hit - y2[i]*mis);
           i_save = i;
         }
-        
       }
       
       res.score =D[i_save] * hit - y2[i_save]*mis ;
@@ -300,77 +407,79 @@ static
     return(res);
   }
 
-/*
-SEXP ks_genescore_c(SEXP n_x, SEXP y)
-{
-    int N = LENGTH(y);
-    int nx = asInteger(n_x);
-    struct ks_score ks;
-    SEXP ans;
-    int *yarray;
-    y = PROTECT(coerceVector(y, INTSXP));
-    yarray = INTEGER(y);
-    ks = ks_genscore(nx, N, yarray);
-    PROTECT(ans = allocVector(REALSXP, 2 ));
-    REAL(ans)[0] = ks.score;
-    REAL(ans)[1] = ks.pvalue;
-    UNPROTECT(2);
-    return (ans);
-}
-*/
-  
-  
+
+// ---------------------------------------------------------------------- //
+//                                                                        //
+//  Compute directional KS scores for each row of a given binary matrix   //
+//
+// INPUT:
+//  mat - matrix of binary features to compute row-wise scores for 
+//        based on the  Kolmogorov-Smirnov test
+//  alt - an integer value specifying the alternative hypothesis, 
+//        must be one of 0: "two.sided", +1: "less"; -1: "greater". 
+//  w   - a vector of weights to use if performing a weighted-KS test. 
+//        Default is NULL.
+//
+// OUTPUT:
+// A 2 x N matrix (N = number of rows in matrix) with 
+//         the first and second rows corresponding to 
+//         the KS scores and p-values, respectively 
+// 
+// ------------------------------------------------------------------------ //
 SEXP ks_genescore_mat_(SEXP mat, SEXP w, SEXP alternative)
 {
-    struct ks_score ks;
-    SEXP Rdim, ans;
-    double *ymat, *rans;
-    int *weight;
-    int N, ncol, nrow, nw, i, j;
-    int *yarray;
+  struct ks_score ks;
+  double *ymat, *rans;
+  int *weight;
+  int N, ncol, nrow, nw, i, j;
+  int *yarray;
+  int alt;
+  SEXP Rdim, ans;
+  
+  ymat = REAL(mat);
+  
+  Rdim = getAttrib(mat, R_DimSymbol);
+  nrow = INTEGER(Rdim)[0];
+  ncol = INTEGER(Rdim)[1];
+  nw = LENGTH(w);
+  //printf("length of w: %d\n", nw);
+  
+  if (LENGTH(alternative) > 0) 
+  {
+    alt= INTEGER(alternative)[0]; /* +1 = left-sided, 0 = double sided, -1 = right sided */
+  } else 
+  {
+    alt = 1;
+  }
+  
+  if (nw > 0 && nw != ncol) 
+  {
+    return(R_NilValue);
+  }
+  
+  if (nw > 0) weight = INTEGER(w);
+  
+  yarray = (int*) malloc( ncol * sizeof(int) );
+  PROTECT(ans = allocMatrix(REALSXP, 2, nrow ));
+  rans = REAL(ans);
+  
+  for (i = 0; i < nrow; i++)
+  {
     
-    int alt;
-    
-    ymat = REAL(mat);
-    
-    Rdim = getAttrib(mat, R_DimSymbol);
-    nrow = INTEGER(Rdim)[0];
-    ncol = INTEGER(Rdim)[1];
-    nw = LENGTH(w);
-    //printf("length of w: %d\n", nw);
-    
-    if (LENGTH(alternative) > 0) {
-       alt= INTEGER(alternative)[0]; /* +1 = left-sided, 0 = double sided, -1 = right sided */
-    } else {
-      alt = 1;
-    }
-
-    if (nw > 0 && nw != ncol) {
-      return(R_NilValue);
-    }
-    if (nw > 0) weight = INTEGER(w);
-
-    
-    yarray = (int*) malloc( ncol * sizeof(int) );
-    PROTECT(ans = allocMatrix(REALSXP, 2, nrow ));
-    rans = REAL(ans);
-
-    for (i = 0; i < nrow; i++)
+    for (j = 0, N = 0; j < ncol; j++)
     {
-
-      for (j = 0, N=0; j < ncol; j++){
-        if (ymat[ j * nrow + i] >0.1 ) { 
-            yarray[N++] = j+1;
-         }
-       }
-       ks = ks_genscore(ncol, N, yarray, nw, weight, alt);
-       rans[i * 2] = ks.score;
-       rans[i * 2 + 1] = ks.pvalue;
+      if (ymat[ j * nrow + i] >0.1 ) { 
+        yarray[N++] = j+1;
+      }
     }
-    free(yarray);
-
-    UNPROTECT(1);
-    return (ans);
+    ks = ks_genscore(ncol, N, yarray, nw, weight, alt);
+    rans[i * 2] = ks.score;
+    rans[i * 2 + 1] = ks.pvalue;
+  }
+  free(yarray);
+  
+  UNPROTECT(1);
+  return (ans);
 }
 
 

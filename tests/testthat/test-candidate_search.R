957b644e4805113c6237accfa3b44e60f2d869b5
Katia Bulekova <ktrn@bu.edu>
2022-05-01 21:41:00 -0400

    1. Remove kde2_wrap as this funcion is not yet finished 2. add additional unit tests for both ks and wilcox fincutions

--- tests/testthat/test-candidate_search.R
+++ tests/testthat/test-candidate_search.R
@@ -5,3 +5,46 @@ test_that("cadra_search receives correct input arguments ", {
 
   
 })
+
+
+test_that("cadra_search returns expected result ",{
+  
+  # Load R library
+  suppressPackageStartupMessages(library(Biobase))
+  
+  # Load pre-computed expression set
+  data(sim.ES)
+  
+  # set seed
+  set.seed(123)
+  
+  # Provide a vector of continuous scores for a target profile with names to each score value 
+  input_score = rnorm(n = ncol(sim.ES))
+  names(input_score) <- colnames(sim.ES)
+  
+  # Define additional parameters and run the function
+  ks <- candidate_search(
+    ES = sim.ES, input_score = input_score, method = "ks", 
+    alternative = "less", weights = NULL, metric = "pval", 
+    search_start = NULL, search_method = "both", 
+    max_size = 7, best_score_only = FALSE
+  )
+  testthat::expect_type(ks, "list")
+  testthat::expect_length(ks, 3)
+  testthat::expect_snapshot(ks)
+  
+  
+  # Run candidate_search with wilcox method
+  wilcox <- candidate_search(
+    ES = sim.ES, input_score = input_score, method = "wilcox",
+    alternative = "less", weights = NULL, metric = "pval",
+    search_start = NULL, search_method = "both",
+    max_size = 7, best_score_only = FALSE
+  )
+
+  testthat::expect_type(wilcox, "list")
+  testthat::expect_length(wilcox, 3)
+  testthat::expect_snapshot(wilcox)
+  
+  
+})
